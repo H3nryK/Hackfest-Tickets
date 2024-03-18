0103d7a980305ffe72e5e9c7bc1628e4395ebeee
WachiaLeila <leilamkawazungu@gmail.com>
2024-03-18 11:24:32 +0300

    emailing

--- ticket/views.py
+++ ticket/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from .utils import generate_qr_code
 from django.urls import reverse
+from django.template.loader import render_to_string
+from django.conf import settings
+from django.core.mail import send_mail
 
 def register(request):
     if request.method == 'POST':
@@ -21,6 +24,29 @@ def register(request):
             school = user_school,
             language = user_languages,
             year = user_year
+        )
+
+        # Generate QR code URL
+        qr_code_url = request.build_absolute_uri(reverse('ticket', kwargs={'ticket_id': ticket.id}))
+
+        email_content = render_to_string('registration_email.html', { 
+            'name' : ticket.name,
+            'email': ticket.email,
+            'team' : ticket.team,
+            "course" : ticket.course,
+            "school" : ticket.school,
+            "language" : ticket.language,
+            "year" :ticket.year,
+            'qr_code_url': qr_code_url,
+            'ref_code': ticket.ref_code
+        })
+
+        send_mail(
+            'Registration Details',
+            email_content,
+            settings.DEFAULT_FROM_EMAIL,
+            [ticket.email],
+            fail_silently=False,
         )
 
         if created:
